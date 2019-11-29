79f402adb1ee7a3730b7fcd8e41029c867e42c4e
kapiljhajhria <Aerospace44>
2019-11-29 12:06:37 +0530

    stage 1 of stretch

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,18 @@
 import 'day40.dart';
+
 // Part 1
 // Tower of Hanoi - 3 Moves
 // Write a function which will check win in 3 moves, use the code you wrote yesterday
 List<List> threeMovesToWin(List<List> towers) {
   List<List> allPossibleMoves = hanoiPossibleMoves(towers);
   for (int i = 0; i < allPossibleMoves.length; i++) {
-    if (twoMoveToWin(allPossibleMoves[i])!=null) {
+    if (twoMoveToWin(allPossibleMoves[i]) != null) {
       return allPossibleMoves[i];
     }
   }
   return null;
 }
+
 // Part 2
 // Tower of Hanoi - N Moves
 // Write a function which will check win in N moves for a given state of Tower of Hanoi
@@ -21,7 +23,7 @@ List<List> nMoveToWin(List<List> towers, int n) {
   }
   List<List> allPossibleMoves = hanoiPossibleMoves(towers);
   for (int i = 0; i < allPossibleMoves.length; i++) {
-    if (nMoveToWin(allPossibleMoves[i], n-1)!=null) {
+    if (nMoveToWin(allPossibleMoves[i], n - 1) != null) {
       return allPossibleMoves[i];
     }
   }
@@ -29,22 +31,31 @@ List<List> nMoveToWin(List<List> towers, int n) {
   return null;
 }
 
+allMovesToWin(List<List> towers) {
+  ///-------------
+  ///loop 1
+  int n = 1;
+  do {
+    if (nMoveToWin(towers, n) != null) {
+      print('n here is $n');
+      print(nMoveToWin(towers, n));
+    }
+    n++;
+  }while(nMoveToWin(towers, n-1) == null);
+
+  ///---------------
+}
+
 // Stretch: Show all moves which lead to win, given a state.
 main() {
-  List<List> GameInstance0101 = [[1],[],[2,3, 4]];
-  print(nMoveToWin(GameInstance0101,1));
-
-  List<List> GameInstance0201 = [
-    [1],
-    [2],
-    [3, 4]
-  ];
-  print(nMoveToWin(GameInstance0201,2));
-  List<List> GameInstance0301 = [
-    [1,2],
+  print(allMovesToWin([
+    [1, 2, 3, 4],
     [],
-    [3, 4]
-  ];
-  print(nMoveToWin(GameInstance0301,3));
-  print(nMoveToWin([[1,2,3,4],[],[]],15));
+    []
+  ]));
+//  print(nMoveToWin([
+//    [1, 2, 3, 4],
+//    [],
+//    []
+//  ], 15));
 }
